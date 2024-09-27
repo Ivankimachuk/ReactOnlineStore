422320e3849b9f057d2baaf2eea94614fbc07e46
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2024-09-27 15:27:29 +0300

    Added basic styles

--- shop/src/components/Header.js
+++ shop/src/components/Header.js
@@ -5,6 +5,11 @@ export default function Header() {
     <header>
       <div>
         <span className="logo">House Staff</span>
+        <ul className="nav">
+          <li>About Us</li>
+          <li>Contacts</li>
+          <li>Account </li>
+        </ul>
       </div>
       <div className="presentation"></div>
     </header>
